bfb731ceb0fa620a3af851d0f0d08e82dae342bb
amenitraa <amenitra.anderson@insightglobal.com>
2026-04-03 16:37:11 -0400

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,5 @@
-export const SUPABASE_URL = window.SUPABASE_URL || 'YOUR_SUPABASE_URL';
-export const SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || 'YOUR_SUPABASE_ANON_KEY';
+export const SUPABASE_URL = window.SUPABASE_URL || 'https://btulujmzywugnubopxif.supabase.co';
+export const SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || 'sb_publishable_JbetOpbjfF0Lgb400tecNQ_SP-LzyA-';
 
 let supabase = null;
 
